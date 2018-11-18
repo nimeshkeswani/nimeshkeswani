d09466ec76f33f99491d3e785c879518b8259bb3
Nimesh Keswani <nimeshkeswani@hotmail.com>
2018-11-17 22:24:13 -0800

    add BrowserRouter to App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import './App.css';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+	<Router basename={process.env.PUBLIC_URL}>
+        <Fragment>
         <header className="App-header">
 	Nimesh Keswani
         </header>
+	</Fragment>
+	</Router>
       </div>
     );
   }
