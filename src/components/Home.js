0520c3f4214afad48caad81a0b98641271d73382
Nimesh Keswani <nimeshkeswani@hotmail.com>
2018-11-18 15:59:53 -0800

    separate css files for components

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,14 +1,17 @@
 import React, { Component, Fragment } from 'react'
 import Links from './Links'
+import '../styles/Home.css';
 
 class Home extends Component {
 	render () {
 		return (
 			<Fragment>
-				<header className="App-header">
-					Nimesh Keswani
+				<header className="Home-header">
+					<text>Nimesh Keswani</text>
+					<Links />
         </header>
-				<Links />
+				<body className="Home-body">
+				</body>
       </Fragment>
 		)
 	}
--- src/components/Links.js
+++ src/components/Links.js
@@ -1,16 +1,17 @@
 import React, { Component } from 'react'
 import github from '../assets/images/github-sign.svg'
 import linkedin from '../assets/images/linkedin-sign.svg'
+import '../styles/Links.css';
 
 class Links extends Component {
 	render () {
 		return (
-      <div className="App-body">
+      <div className="Links">
         <a href="https://github.com/nimeshkeswani">
-          <img className="App-icon-image" src={github} alt="github" />
+          <img className="Link-icon-image" src={github} alt="github" />
         </a>
         <a href="https://www.linkedin.com/in/nimeshkeswani/">
-          <img className="App-icon-image" src={linkedin} alt="linkedin" />
+          <img className="Link-icon-image" src={linkedin} alt="linkedin" />
         </a>
       </div>
 		)
