4b6d2dcc64281f7952d8cf9f49a95746fc5c6895
Nimesh Keswani <nimeshkeswani@hotmail.com>
2018-11-18 15:34:00 -0800

    change layout of Home component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,23 +1,23 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react'
 import github from '../assets/images/github-sign.svg'
 import linkedin from '../assets/images/linkedin-sign.svg'
 
 class Home extends Component {
 	render () {
 		return (
-			<div>
-			<header className="App-header">
-				Nimesh Keswani
-        	</header>
-        	<div className="App-body">
+			<Fragment>
+				<header className="App-header">
+					Nimesh Keswani
+        </header>
+        <div className="App-body">
         		<a href="https://github.com/nimeshkeswani">
         			<img className="App-icon-image" src={github} alt="github" />
         		</a>
         		<a href="https://www.linkedin.com/in/nimeshkeswani/">
         			<img className="App-icon-image" src={linkedin} alt="linkedin" />
         		</a>
-        	</div>
-        	</div>
+        </div>
+      </Fragment>
 		)
 	}
 }
