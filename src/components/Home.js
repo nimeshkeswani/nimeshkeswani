fade0f5f53bfe4b7f819a76df24a1fdbde8f8ced
Nimesh Keswani <nimeshkeswani@hotmail.com>
2018-11-18 01:16:21 -0800

    use local image files for icons

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import test from './test.png'
+import github from '../assets/images/github-sign.svg'
+import linkedin from '../assets/images/linkedin-sign.svg'
 
 class Home extends Component {
 	render () {
@@ -10,10 +11,10 @@ class Home extends Component {
         	</header>
         	<div className="App-body">
         		<a href="https://github.com/nimeshkeswani">
-        			<img className="App-icon-image" src="https://image.flaticon.com/icons/svg/25/25657.svg" alt="github" />
+        			<img className="App-icon-image" src={github} alt="github" />
         		</a>
         		<a href="https://www.linkedin.com/in/nimeshkeswani/">
-        			<img className="App-icon-image" src="https://image.flaticon.com/icons/svg/25/25320.svg" alt="linkedin" />
+        			<img className="App-icon-image" src={linkedin} alt="linkedin" />
         		</a>
         	</div>
         	</div>
