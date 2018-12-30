c2d581bd5eeb1d69b527d541c97c9fa505d4db27
Nimesh Keswani <nimeshkeswani@hotmail.com>
2018-12-29 16:24:00 -0800

    use github apis to show projects

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,34 +1,54 @@
 import React, { Component } from 'react'
 import '../styles/Projects.css'
 import Project from './Project'
+import axios from 'axios'
 
 class Projects extends Component {
-	render () {
-		return (
-			<div className='Projects'>
+
+  state = {
+    frontendRepos: [],
+    backendRepos: []
+  }
+
+  async componentDidMount () {
+    const repos = await axios.get('https://api.github.com/users/nimeshkeswani/repos')
+    this.setState({ frontendRepos: repos.data.filter(repo => ['mobile-flashcards', 'nimeshkeswani.github.io', 'my-reads', 'would-you-rather'].includes(repo.name)) })
+    this.setState({ backendRepos: repos.data.filter(repo => ['nimesh-server'].includes(repo.name)) })
+  }
+
+  render () {
+    return (
+      <div className='Projects'>
         <div className='ProjectsCategory'>
-        <h3>Frontend</h3>
-        <Project
-          title='Would You Rather?'
-          description='A Would You Rather App using React & Redux'
-          githubLink='https://github.com/nimeshkeswani/would-you-rather'
-          externalLink='https://nimeshkeswani.github.io/would-you-rather' />
-        <Project
-          title='My Reads'
-          description='A Book Tracking App using React'
-          githubLink='https://github.com/nimeshkeswani/my-reads'
-          externalLink='https://nimeshkeswani.github.io/my-reads' />
+          <h3>Frontend</h3>
+          {this.state.frontendRepos.map(repo => (
+            <Project
+              key={repo.name}
+              title={repo.name}
+              description={repo.description}
+              githubLink={repo.html_url}
+              externalLink={
+                ['nimeshkeswani.github.io', 'my-reads', 'would-you-rather'].includes(repo.name) 
+                ? `https://nimeshkeswani.github.io/${repo.name}`
+                : ''
+              }
+            />
+          ))}
         </div>
         <div className='ProjectsCategory'>
-        <h3>Backend</h3>
-        <Project
-          title='Backend Server'
-          description='A Backend Server using Node.js'
-          githubLink='https://github.com/nimeshkeswani/nimesh-server' />
+          <h3>Backend</h3>
+          {this.state.backendRepos.map(repo => (
+            <Project
+              key={repo.name}
+              title={repo.name}
+              description={repo.description}
+              githubLink={repo.html_url}
+              externalLink='' />
+          ))}
         </div>
       </div>
-		)
-	}
+    )
+  }
 }
 
 export default Projects
